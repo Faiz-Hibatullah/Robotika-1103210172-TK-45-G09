2bfe50c12be278112137ca70ad5d87c1005a5580
Faiz-Hibatullah <93574053+Faiz-Hibatullah@users.noreply.github.com>
2024-10-14 22:33:02 +0700

    Add files via upload

---
+++ Week 3/Simulasi Gerakan Melingkar/looping_controller.py
@@ -0,0 +1,23 @@
+from controller import Robot
+
+TIME_STEP = 64  # Interval waktu dalam simulasi
+MAX_SPEED = 6.28  # Kecepatan maksimum motor
+
+# Inisialisasi robot
+robot = Robot()
+
+# Mendapatkan referensi motor kiri dan kanan
+leftMotor = robot.getDevice('left wheel motor')
+rightMotor = robot.getDevice('right wheel motor')
+
+# Menyetel posisi motor agar bisa berputar tanpa batas
+leftMotor.setPosition(float('inf'))
+rightMotor.setPosition(float('inf'))
+
+# Menyetel kecepatan motor agar bergerak maju
+leftMotor.setVelocity(0.5*MAX_SPEED)
+rightMotor.setVelocity(MAX_SPEED)
+
+# Loop utama simulasi
+while robot.step(TIME_STEP) != -1:
+    pass  # Robot akan terus bergerak maju
